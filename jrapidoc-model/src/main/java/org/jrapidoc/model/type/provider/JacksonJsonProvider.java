c712c88fb6a95006fb3eeb3aa29956094de3d674
sarzwest <jiratom@seznam.cz>
2015-04-25 16:57:01 +0200

    Added better support for ParameterizedType and TypeVariable

--- jrapidoc-model/src/main/java/org/jrapidoc/model/type/provider/JacksonJsonProvider.java
+++ jrapidoc-model/src/main/java/org/jrapidoc/model/type/provider/JacksonJsonProvider.java
@@ -7,6 +7,7 @@ import org.jrapidoc.model.type.provider.converter.JacksonToJrapidocProcessor;
 import sun.reflect.generics.reflectiveObjects.TypeVariableImpl;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.TypeVariable;
 import java.util.Map;
 
 /**
@@ -40,29 +41,25 @@ public class JacksonJsonProvider extends TypeProvider {
             if (((Class) genericType).isArray()) {
                 javaType = objectMapper.getTypeFactory().constructArrayType(((Class) genericType).getComponentType());
             } else {
-//            jedna se o custom type
                 javaType = objectMapper.getTypeFactory().constructParametrizedType((Class) genericType, (Class) genericType, new Class[]{});
             }
         } else if (genericType instanceof ParameterizedType) {
-            //jedna se o parametrizovany typ
             ParameterizedType paramType = (ParameterizedType) genericType;
             java.lang.reflect.Type[] genericTypes = paramType.getActualTypeArguments();
-            //Kdyz je typ genericky napr <T>, nahrad ho za Object
             for (int i = 0; i < genericTypes.length; i++) {
                 if (genericTypes[i] instanceof TypeVariableImpl) {
-                    genericTypes[i] = Object.class;
+                    genericTypes[i] = ((TypeVariable) genericTypes[i]).getBounds()[0];
                 }
             }
             JavaType[] javaTypes = new JavaType[genericTypes.length];
             for (int i = 0; i < javaTypes.length; i++) {
                 javaTypes[i] = createJavaType(genericTypes[i]);
             }
-            //prochazej generictypes a vytvarej z kazdeho JavaType a davej do pole
-//            Class[] typeArguments = Arrays.copyOf(genericTypes, genericTypes.length, Class[].class);
-//          bad  javaType = JacksonUtil.objectMapperInstance().getTypeFactory().constructParametrizedType(genericType, genericType, typeArguments);
             javaType = objectMapper.getTypeFactory().constructParametrizedType((Class) paramType.getRawType(), (Class) paramType.getRawType(), javaTypes);
-        } else if (genericType instanceof TypeVariableImpl) {
-            javaType = objectMapper.getTypeFactory().constructParametrizedType(Object.class, Object.class, new Class<?>[]{});
+        } else if (genericType instanceof TypeVariable) {
+            TypeVariable typeVariable = ((TypeVariable) genericType);
+            java.lang.reflect.Type bound = typeVariable.getBounds()[0];
+            javaType = createJavaType(bound);
         }
         return javaType;
     }
